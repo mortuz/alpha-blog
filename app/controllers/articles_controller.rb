736adec7380bcaedb9f33dd06d4c6218fe1a8277
Mortuz <mortuzalam@gmail.com>
2018-02-23 07:31:26 +0000

    Edit, show article template and controller

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,50 @@
+class ArticlesController < ApplicationController
+  
+  def index
+    
+    @articles = Article.all
+    
+  end
+  
+  def new
+    @article = Article.new
+  end
+  
+  def create
+    # render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    
+    if @article.save
+      flash[:notice] = "Article is successfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+      
+  end
+  
+  def show
+    @article = Article.find(params[:id])
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    
+    if @article.update(article_params)
+      flash[:notice] = "Article successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
+  
+end
