a7b6c5dfea67dfe6e4ea68a2733e054c798f29ab
Mortuz <mortuzalam@gmail.com>
2018-02-23 18:00:10 +0000

    REfactored atricles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :destroy, :show]
   
   def index
     
@@ -24,16 +25,14 @@ class ArticlesController < ApplicationController
   end
   
   def show
-    @article = Article.find(params[:id])
+    
   end
   
   def edit
-    @article = Article.find(params[:id])
+   
   end
   
   def destroy
-    @article = Article.find(params[:id])
-    
     @article.destroy
     
     flash[:notice] = "Article successfully deleted"
@@ -41,7 +40,6 @@ class ArticlesController < ApplicationController
   end
   
   def update
-    @article = Article.find(params[:id])
     
     if @article.update(article_params)
       flash[:notice] = "Article successfully updated"
@@ -52,6 +50,9 @@ class ArticlesController < ApplicationController
   end
   
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
     def article_params
       params.require(:article).permit(:title, :description)
     end
