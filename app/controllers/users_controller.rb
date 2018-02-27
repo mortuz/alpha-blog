e2a05c7091dff1af5989319a3cff29cb1cc2117d
Mortuz <mortuzalam@gmail.com>
2018-02-27 18:43:55 +0000

    signin upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     
     if @user.save
-      flash[:success] = "Welcomr to the Alpha Blog #{@user.username}"
-      redirect_to articles_path
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
+      redirect_to user_path(@user)
     else
       render 'new'
     end
